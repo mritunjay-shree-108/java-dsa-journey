73bcf723224744fbca84c6330b92461ffc492e2e
mritunjay-shree-108 <mritunjay.shree.1089@gmail.com>
2026-05-06 13:30:14 +0530

    Add maxSumSubarray class to calculate maximum subarray sum
    Implemented a function to find the maximum sum of a subarray using a brute-force approach.

---
+++ Arrays/maxSumSubarray.java
@@ -0,0 +1,38 @@
+/*
+Problem: Maximum Sum of Sub-Array
+Time Complexity: O(n^3)
+
+Approach:
+Traverse the array and calculate the current sum and update maximum sum if greater
+Return the maximum sum.
+*/
+
+
+import java.util.*;
+
+public class maxSumSubarray {
+
+    // function to calculate max sum
+    public static int maxSum(int arr[]){
+        int max = Integer.MIN_VALUE;                  // -infinity
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i; j<arr.length; j++){
+                int currSum = 0;
+                for(int k = i; k<=j;k++){
+                    currSum += arr[k];
+                }
+                if(currSum > max){
+                    max = currSum;
+                }
+            }
+        }
+        return max;
+    }
+
+    // main function
+    public static void main(String[] args){
+        int arr[] = {1,2,3,-2,5};
+        System.out.println("Maximum sum of a subarray is : " + maxSum(arr));
+    }
+    
+}
